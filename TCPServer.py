c330f23e63e9e4bbf6362a8f81478345916bc632
Pips <filipabartlett@gmail.com>
2023-09-28 16:28:54 +0100

    Creating TCP server
    TCP script

---
+++ TCPServer.py
@@ -0,0 +1,23 @@
+import socket
+
+serversocket = socket.socket(
+socket.AF_INET, socket.SOCK_STREAM)
+
+
+host = socket.gethostname()
+port = 444 
+
+serversocket.bind((host, port)) 
+
+
+serversocket.listen(3)
+
+
+while True:
+
+    clientsocket, address = serversocket.accept()
+    print("Connection has been established from " &str(address))
+
+message = "Hello!, Thank you for connecting to the server" + "\r\n"
+clientsocket.send(message)
+clientsocket.close() 
